fe269ef7dc6ec754be0bb1b914c3607d2ff2b529
Lalit doriya <158010530+LalitFD@users.noreply.github.com>
2024-07-13 01:47:20 +0000

    datatypes

---
+++ .devcontainer/basics/datatypes.js
@@ -0,0 +1,24 @@
+// alert(Hello) we are using nodejs not browser ..
+
+console.log(3+3)
+
+console.log("Doriya")
+
+let name ="Lalit"   //String
+let age=20          //number 
+let isLoggedIn=false   //Boolean
+let state=null
+
+
+//number => 2 to the power 53
+//bigint => iit is very big number
+//String => name place doule code me jo likhnge wo String hoga 
+// Boolean =>true /false 
+// null => Standalone value 
+// undefined =>
+// Sysmbol => unique
+// Object => aage baat karenge 
+
+console.log(typeof age)
+console.log(typeof null)
+console.log(typeof undefined)
---
+++ .devcontainer/basics/variable.js
@@ -0,0 +1,13 @@
+const accountId=45523
+let accountEmail="Lalit@123"
+var accountPassword="123456"
+accountCity="jaipur"
+
+//accountId=454
+accountEmail="raju@gmail.com"
+accountPassword=45646
+accountCity="indore"
+
+
+console.log(accountId)
+console.table([accountEmail,accountPassword,accountCity])
