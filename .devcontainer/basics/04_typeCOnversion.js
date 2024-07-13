656cdac0e350661c33abbd82421718debd63a95c
Lalit doriya <158010530+LalitFD@users.noreply.github.com>
2024-07-13 02:47:00 +0000

    conversion

---
+++ .devcontainer/basics/04_typeCOnversion.js
@@ -0,0 +1,20 @@
+// let score = "33"
+// console.log(typeof score)
+// console.log(typeof (score))
+
+// let valueInNumber = Number(score)
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber) // string ko number change  kara hai 
+
+// let isLoggedIn = 2
+// let BooleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(BooleanIsLoggedIn) //number ko boolean mechange kara hai 
+
+//  1 => true
+//  0 = > false
+//  "" => false
+//  "lalit" => true
+
+let somenumber=33
+let stringumber=String(somenumber)
+console.log (typeof stringumber)         //yaha per hmne number ko string me change kara hai 
