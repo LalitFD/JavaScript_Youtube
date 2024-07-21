0c821c2fa414e682701592cd412303d62f617cf4
Lalit doriya <158010530+LalitFD@users.noreply.github.com>
2024-07-21 05:22:39 +0000

    new

---
+++ .devcontainer/basics/05_operations.js
@@ -0,0 +1,21 @@
+// let value=5
+// let negvalue=-value
+// console.log(negvalue)
+
+// let str1="hello"
+// let str2=" Lalit"
+// let str3=str1+str2
+// console.log(str3)
+
+// console.log("1"+2)
+// console.log(1+"2")
+// console.log("1"+2+2)
+// console.log(1+2+"2")
+
+let gamecounter=100
+gamecounter++
+console.log(gamecounter)
+
+let counter=212
+++counter
+console.log(counter)
---
+++ .devcontainer/basics/11_Array.js
@@ -0,0 +1,36 @@
+const myArr = [1, 2, 3, 4, 5, 6]
+console.log(myArr[1])
+
+//Array /Method
+myArr.push(6)
+myArr.push(4)
+myArr.pop()
+
+console.log(myArr)
+myArr.unshift(9)
+console.log(myArr)
+
+
+
+console.log(myArr.includes(9));
+console.log(myArr.indexOf(3));
+
+const newArr = myArr.join()
+
+console.log(myArr);
+console.log( newArr);
+
+
+// slice, splice
+
+console.log("A ", myArr);
+
+const myn1 = myArr.slice(1, 3)
+
+console.log(myn1);
+console.log("B ", myArr);
+
+
+const myn2 = myArr.splice(1, 3)
+console.log("C ", myArr);
+console.log(myn2);
